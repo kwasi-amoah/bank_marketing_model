e7d730b6e03ca7f3944ae3afd2a203c146edafa1
kwasi-amoah <153782204+kwasi-amoah@users.noreply.github.com>
2025-06-06 00:42:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,11 @@ if uploaded_file is not None:
 
         # Create a DataFrame with a custom column name
         pred_df = pd.DataFrame(labels, columns=["predicted y"])
-        st.dataframe(pred_df, hide_index=True)
+        
+        # Display predictions
+        st.write("### Predictions")
+        st.dataframe(pred_df)
+        
 
         
 
